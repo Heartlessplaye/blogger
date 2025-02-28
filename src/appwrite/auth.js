3c085350888153ae08bd31d3b678c912b0d0f0a7
Anuj Kumar Jha <anujkumarjha4303@gmail.com>
2025-03-01 04:42:12 +0530

    setup redux authReducer

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-useless-catch */
-import conf from "../conf";
+import conf from "../conf/conf";
 import { Client, Account, ID } from "appwrite";
 
 export class AuthService {
@@ -9,55 +9,55 @@ export class AuthService {
   constructor() {
     this.client
       .setEndpoint(conf.appwriteUrl)
-      .setProject(conf.appwriteProjectId);
+      .setProject(conf.appwriteProjectId)
+      .setKey(process.env.API_KEY);
 
     this.account = new Account(this.client);
   }
 
-  async createAccount({email, password, name}) {
-    
-    try{
-        const userAccount = await this.account.create(ID.unique(),email, password, name);
-        if(userAccount) {
-            // call another method 
-          return this.login({email,password});
-        }
-        else {
-            return userAccount;
-        }
-    }
-    catch (error){
-        throw error;
-    }
-  }
-
-  async login ({email, password}){
-    try{
-        return await this.account.createEmailPasswordSession(email,password); 
-
-    }catch(error) {
-        throw error;
-    }
-  }
-
-  async getCurrentUser(){
-    try{
-       return await this.account.get();
-    }catch(error){
-        throw error;
-    }
-  }
-
-  async logout(){
+  async createAccount({ email, password, name }) {
     try {
-        await this.account.deleteSessions();
-    }
-    catch(error) {
-        console.log("Appwrite service :: logout :: error ", error);
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        // call another method
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      console.log("Appwrite service :: createAccount :: error ", error);
     }
   }
 
-  
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      console.log("Appwrite service :: login :: error ", error);
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite service :: getCurrentUser :: error ", error);
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite service :: logout :: error ", error);
+    }
+  }
 }
 
 const authService = new AuthService();
